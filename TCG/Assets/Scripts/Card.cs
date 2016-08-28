0b56241693752cc667275703ee2a603777e00689
Fabricio Guedes <fabricio-gf@hotmail.com>
2016-08-28 15:41:12 -0300

    Add Unity Project

---
+++ TCG/Assets/Scripts/Card.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+
+    string title;
+    float manaCostF;
+    float manaCostW;
+    float manaCostE;
+
+    //image
+    
+    string description;
+    //string flavorText;?
+}
