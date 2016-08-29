8715a2323449053ee7b9bc58376b17e8db6cccb0
wqferr <wqferr@gmail.com>
2016-08-28 22:55:51 -0300

    Add proper methods to TerrainCard events

--- TCG/Assets/Scripts/TerrainCard.cs
+++ TCG/Assets/Scripts/TerrainCard.cs
@@ -7,5 +7,17 @@ public class TerrainCard : Card {
 
     [HideInSpector]
     public delegate void OnCreatureAttack();
+	
+	public void CreatureAttacked() {
+		var handler = this.OnCreatureAttacked;
+        if (handler != null)
+            handler();
+	}
+	
+	public void CreatureAttack() {
+		var handler = this.OnCreatureAttack;
+        if (handler != null)
+            handler();
+	}
 
 }
