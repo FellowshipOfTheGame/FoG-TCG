14e824b03bc81c11f9b7a9f140c8ee660170e037
Fabricio <fabricio-gf@hotmail.com>
2016-10-13 10:18:21 -0300

    Add full hand mechanic
    If player draws when he has 10 cards, the card drawn is discarded instead.

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -8,7 +8,7 @@ public class CardManager : MonoBehaviour {
     public Canvas canvas;
 
     List<CardScriptableObject> deck = new List<CardScriptableObject>();
-    int deckSize = 10;
+    int deckSize = 30;
     List<GameObject> hand = new List<GameObject>();
     int handSize = 0;
     public GameObject card;
@@ -19,13 +19,13 @@ public class CardManager : MonoBehaviour {
     // Use this for initialization
     void Start () {
 
-        for(int i = 0; i < 5; i++)
+        for(int i = 0; i < 15; i++)
         {
             deck.Add(cartaGuaxininja);
             deck.Add(cartaYoko);
         }
 
-        for(int i = 0; i < 10; i++)
+        for(int i = 0; i < 30; i++)
         {
             CardScriptableObject temp;
             int randSpace = Random.Range(0, deckSize);
@@ -33,38 +33,35 @@ public class CardManager : MonoBehaviour {
             deck[randSpace] = deck[i];
             deck[i] = temp;
         }
-
-        /*
-        GameObject panel1 = (GameObject)Instantiate(card, Vector3.zero, Quaternion.identity, canvas.transform);
-        panel1.transform.SetParent(canvas.transform, false);
-        panel1.transform.position = canvas.transform.position + new Vector3(75, 0);
-        panel1.GetComponent<AddCardInformation>().card = cartaGuaxininja;
-
-        GameObject panel2 = (GameObject)Instantiate(card, Vector3.zero, Quaternion.identity, canvas.transform);
-        panel2.transform.SetParent(canvas.transform, false);
-        panel2.transform.position = canvas.transform.position + new Vector3(-75, 0);
-        panel2.GetComponent<AddCardInformation>().card = cartaYoko;
-        */
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && deckSize > 0)
+        if (Input.GetButtonDown("Fire1") && deckSize > 0)
         {
-            hand.Add((GameObject)Instantiate(card, Vector3.zero, Quaternion.identity, canvas.transform)); 
-            hand[handSize].transform.SetParent(canvas.transform, false);
-            if (handSize == 0)
+            if (handSize < 10)
             {
-                hand[handSize].transform.position = canvas.transform.position + new Vector3(-350, 0);
+                hand.Add((GameObject)Instantiate(card, Vector3.zero, Quaternion.identity, canvas.transform));
+                hand[handSize].transform.SetParent(canvas.transform, false);
+                if (handSize == 0)
+                {
+                    hand[handSize].transform.position = canvas.transform.position + new Vector3(-350, 0);
+                }
+                else
+                {
+                    hand[handSize].transform.position = hand[handSize - 1].transform.position + new Vector3(80, 0);
+                }
+                hand[handSize].GetComponent<AddCardInformation>().card = deck[0];
+                deck.RemoveAt(0);
+                deckSize--;
+                handSize++;
             }
             else
             {
-                hand[handSize].transform.position = hand[handSize - 1].transform.position + new Vector3(80, 0);
+                deck.RemoveAt(0);
+                deckSize--;
+                print("Card discarded!");
             }
-            hand[handSize].GetComponent<AddCardInformation>().card = deck[0];
-            deck.RemoveAt(0);
-            deckSize--;
-            handSize++;
 
         }
     }
