a8656bcef4a44c138d38e67fc04d1d146f090684
Fabricio <fabricio-gf@hotmail.com>
2016-10-26 20:59:22 -0200

    Fix drag offset

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -5,16 +5,20 @@ using System;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    Vector2 mouseOffset;
+
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("OnBeginDrag");
+        mouseOffset = new Vector2(transform.position.x - eventData.position.x, transform.position.y - eventData.position.y);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log("OnDrag");
 
-        this.transform.position = eventData.position;
+        this.transform.position = eventData.position + mouseOffset;
+
     }
 
     public void OnEndDrag(PointerEventData eventData)
