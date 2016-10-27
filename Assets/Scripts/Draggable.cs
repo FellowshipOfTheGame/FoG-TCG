18ab32f79332ad23174a9d51866496685ff3058e
Fabricio <fabricio-gf@hotmail.com>
2016-10-27 16:02:29 -0200

    Add function to change position of cards inside dropzones

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.EventSystems;
 using System;
@@ -8,14 +9,28 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
     Vector2 mouseOffset;
 
     public Transform parentToReturnTo = null;
+    public Transform placeholderParent = null;
+
+    public GameObject placeholder = null;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("OnBeginDrag");
 
+        placeholder = new GameObject();
+        placeholder.transform.SetParent(this.transform.parent);
+        LayoutElement le = placeholder.AddComponent<LayoutElement>();
+        le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
+        le.preferredHeight = this.GetComponent<LayoutElement>().preferredHeight;
+        le.flexibleWidth = 0;
+        le.flexibleHeight = 0;
+
+        placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+
         mouseOffset = new Vector2(transform.position.x - eventData.position.x, transform.position.y - eventData.position.y);
 
         parentToReturnTo = this.transform.parent;
+        placeholderParent = parentToReturnTo;
         this.transform.SetParent(this.transform.parent.parent);
 
         GetComponent<CanvasGroup>().blocksRaycasts = false;
@@ -27,14 +42,57 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 
         this.transform.position = eventData.position + mouseOffset;
 
+        if(placeholder.transform.parent != placeholderParent)
+        {
+            placeholder.transform.parent = placeholderParent;
+        }
+
+        int newSiblingIndex = placeholderParent.childCount;
+
+        for(int i=0; i < placeholderParent.childCount; i++)
+        {
+            if (placeholderParent.GetComponent<DropZone>().vertical == true)
+            {
+                if (this.transform.position.y > placeholderParent.GetChild(i).position.y)
+                {
+
+                    newSiblingIndex = i;
+
+                    if (placeholder.transform.GetSiblingIndex() < newSiblingIndex)
+                    {
+                        newSiblingIndex--;
+                    }
+                    break;
+                }
+            }
+            if (placeholderParent.GetComponent<DropZone>().horizontal == true)
+            {
+                if (this.transform.position.x < placeholderParent.GetChild(i).position.x)
+                {
+
+                    newSiblingIndex = i;
+
+                    if (placeholder.transform.GetSiblingIndex() < newSiblingIndex)
+                    {
+                        newSiblingIndex--;
+                    }
+                    break;
+                }
+            }
+        }
+
+        placeholder.transform.SetSiblingIndex(newSiblingIndex);
+
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("OnEndDrag");
         this.transform.SetParent(parentToReturnTo);
-
+        this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
         GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        Destroy(placeholder);
 
     }
 }
--- Assets/Scripts/DropZone.cs
+++ Assets/Scripts/DropZone.cs
@@ -4,14 +4,33 @@ using UnityEngine.EventSystems;
 
 public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
 
+    public bool vertical = true;
+    public bool horizontal = false;
+
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Debug.Log("OnPointerEnter");
+        if(eventData.pointerDrag == null)
+        {
+            return;
+        }
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null)
+        {
+            d.placeholderParent = this.transform;
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        Debug.Log("OnPointerExit");
+        if (eventData.pointerDrag == null)
+        {
+            return;
+        }
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null && d.placeholderParent == this.transform)
+        {
+            d.placeholderParent = d.parentToReturnTo;
+        }
     }
 
 	public void OnDrop(PointerEventData eventData)
@@ -19,7 +38,7 @@ public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoin
         Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
 
         Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
-        if(d != null)
+        if (d != null)
         {
             d.parentToReturnTo = this.transform;
         }
