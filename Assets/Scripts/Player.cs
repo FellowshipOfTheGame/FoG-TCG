87472acb9343fa2045736f24c97362318aa6b24a
wqferr <wqferr@gmail.com>
2016-11-21 15:11:09 -0200

    Fix capitalization in instance variable names

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,21 +5,21 @@ using System.Collections.Generic;
 public class Player : MonoBehaviour {
 
     public static readonly uint HAND_MAX_SIZE = 10;
+    public List<Card> Hand;
+    public List<Card> Deck;
 
-    public List<Card> hand;
-    public List<Card> deck;
     // TODO socket and stuff
 
     void Start() {
-        hand = new List<Card>();
+        Hand = new List<Card>();
     }
 
     public bool DrawCard() {
-        if (deck.Count > 0) {
-            Card top = deck[0];
-            deck.RemoveAt(0);
-            if (hand.Count < HAND_MAX_SIZE) {
-                hand.Add(top);
+        if (Deck.Count > 0) {
+            Card Top = Deck[0];
+            Deck.RemoveAt(0);
+            if (Hand.Count < HAND_MAX_SIZE) {
+                Hand.Add(Top);
             }
         }
         // TODO netcode stuff
