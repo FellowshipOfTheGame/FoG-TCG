6893fa2fa7601acd9acc298b3fa18b9e1dc67256
wqferr <wqferr@gmail.com>
2016-11-23 00:05:58 -0200

    Add observer to Board.ToggleTurn

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 
 public class Board : MonoBehaviour {
+
+    public delegate void ToggleTurnDelegate();
+    public event ToggleTurnDelegate ToggleTurnEvent;
+
     private Player[] P;
     public Player P1 {
         get { return P[0]; }
@@ -13,13 +17,29 @@ public class Board : MonoBehaviour {
         get { return P[Turn]; }
     }
     public Player OppositePlayer {
-        get { return P[(Turn+1) % 2]; }
+        get { return P[(Turn+1) & 1]; }
     }
 
     public int Turn;
 
+
     public int ToggleTurn() {
-        Turn = (Turn+1) % 2;
+        foreach (Card[] Row in CurrentPlayer.Field)
+            foreach (Card C in Row)
+                C.OnTurnEnd();
+        Turn = (Turn+1) & 1;
+        OnToggleTurn();
+        foreach (Card[] Row in CurrentPlayer.Field)
+            foreach (Card C in Row)
+                C.OnTurnStart();
+
         return Turn;
     }
+
+    public void OnToggleTurn() {
+        var handler = this.ToggleTurnEvent;
+        if (handler != null)
+            handler();
+    }
+
 }
