3af9da574e1bc01ff0c1378477ddc2f92c991c5b
Fabricio Guedes <fabricio-gf@hotmail.com>
2016-12-10 16:21:49 -0200

    Create DataDeck class and Save and Load methods
    For future reference, temporary file tipe used is .tcgd

---
+++ Assets/Scripts/DataDecks.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public static class DataDecks{
+
+    public static List<Deck> SavedDecks = new List<Deck>();
+
+    public static void Save()
+    {
+        SavedDecks.Add(Deck.current);
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/savedDecks.tcgd");
+        bf.Serialize(file, DataDecks.SavedDecks);
+        file.Close();
+    }
+
+    public static void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "savedDecks.tcgd"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/savedDecks.tcgd", FileMode.Open);
+            DataDecks.SavedDecks = (List<Deck>)bf.Deserialize(file);
+            file.Close();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class Deck {
+
+    public static Deck current;
+    public static string DeckName;
+    public static int DeckSize;
+	public static List<CardInformation> Cards = new List<CardInformation>();
+}
