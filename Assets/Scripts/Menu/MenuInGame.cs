90c0fe65160cd31a62b6853c69ffd55a9f30b135
deiacarpi <deiacarpi@gmail.com>
2017-08-05 17:01:24 -0300

    menu inGame
    com play e pause (util pra versao de testes)

---
+++ Assets/Scripts/Menu/MenuInGame.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInGame : MonoBehaviour {
+
+	public GameObject Config;
+	public GameObject play;
+	public GameObject pause;
+	public GameObject Confirm;
+
+	private bool paused = false;
+	private string saida;
+
+	public void Confirmar() {
+		if(saida.Equals("desistir")) 
+			SceneManager.LoadScene ("Menu");
+
+		if(saida.Equals("sair")) 
+			Application.Quit ();
+	}
+
+	public void Ficar() {
+		Confirm.SetActive (false);
+	}
+
+	public void desistir() {
+		saida = "desistir";
+		Confirm.SetActive (true);
+	}
+
+	public void sair () {
+		saida = "sair";
+		Confirm.SetActive (true);
+	}
+
+	public void playPause() {
+		if (paused) {
+			Time.timeScale = 1;
+			pause.SetActive (true);
+			play.SetActive (false);
+			paused = false;
+		} else {
+			Time.timeScale = 0;
+			pause.SetActive (false);
+			play.SetActive (true);
+			paused = true;
+		}
+	}
+
+	public void showConfig() {
+		Config.SetActive (!Config.activeInHierarchy);
+	}
+}
